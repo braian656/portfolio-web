49cfb286d1ead6e87383394ba09f7e5fe349dd61
Iwantpizza <lxsguaiquian@gmail.com>
2024-08-03 00:42:01 -0300

    nuevos cambios, arreglos

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ const project = [
 		technology: `<span><i class="fa-brands fa-html5"></i></span>
 						<span><i class="fa-brands fa-css3-alt"></i></span>
 							<span><i class="fa-brands fa-js"></i></span>`,
-		link: 'blog.com',
+		link: 'https://ar.pinterest.com/pin/59461657574629728/',
 	},
 	{
 		id: 3,
 		name: 'Snake Game',
 		img: 'img/snake-game.webp',
-		txt: 'Clasico Snake Game',
+		txt: 'Snake game con clasico hecho con javascript',
 		technology: `<span><i class="fa-brands fa-html5"></i></span>
 						<span><i class="fa-brands fa-css3-alt"></i></span>
 							<span><i class="fa-brands fa-js"></i></span>`,
@@ -39,14 +39,9 @@ const menuNav = document.querySelector('.navbar');
 const itemNav = document.querySelectorAll('.item a');
 const containerProject = document.querySelector('.inner-project');
 const txtProject = document.querySelector('.txt-project');
-const downloadCv = document.querySelector('.download-cv');
+const downloadCv = document.getElementById('downloadCv');
 const section = document.querySelector('section');
 
-
-downloadCv.addEventListener('click', ()=>{
-	downloadCv.href = 'CV/Braian-Carranza-CV.pdf';
-	downloadCv.download = 'Braian-Carranza-CV.pdf'
-})
 
 itemNav.forEach((link, i)=>{
 	if(link.href){
@@ -60,7 +55,27 @@ itemNav.forEach((link, i)=>{
 
 document.addEventListener('DOMContentLoaded', ()=>{
 	addProject()
+	
 })
+
+
+const cv = document.getElementById('downloadBtn')
+cv.addEventListener('click',()=>{
+	createButtonCV()
+})
+
+const createButtonCV = ()=>{
+	const a = document.createElement('a')
+	a.classList.add('download-cv')
+	a.href = 'CV/Braian-Carranza-CV.pdf';
+	a.innerHTML = '';
+	a.innerHTML = 'CV'
+	a.download = 'Braian-Carranza-CV.pdf'
+	a.target = '_blank'
+	a.click()
+
+	downloadCv.appendChild(a)
+}
 
 const clean = ()=>{
 
@@ -70,7 +85,7 @@ const clean = ()=>{
 const addActive = ()=>{
 	const cardProject = document.querySelectorAll('.card-project');
 	cardProject[1].classList.add('active')
-	getCard()
+	// getCard()
 	cardProject.forEach((card)=>{
 
 	card.addEventListener('click',()=>{
@@ -81,8 +96,6 @@ const addActive = ()=>{
 		})
 
 		card.classList.add('active')
-		getCard()
-
 
 	});
 })
@@ -99,9 +112,18 @@ function addProject(){
 							<img src=${item.img} alt="">
 						</div>
 						<div class="card-txt">
-							<h1 class="title-txt">${item.name}</h1>
-							<div class="made-with">
-								${item.technology}
+							
+							<div class="card-desc">
+								<h1 class="title-txt">${item.name}</h1>
+								<div class="made-with">
+									${item.technology}
+								</div>
+							</div>
+
+
+							<div class="description">
+								<p>${item.txt}</p>
+								<a class="descr-link" href=${item.link}  target="_blank">see project</a>
 							</div>
 						</div>
 					</article>`
@@ -111,39 +133,6 @@ function addProject(){
 
 	addActive()
 	
-}
-
-function getCard(){
-	const cards = document.querySelectorAll('.card-project');
-	for(let i = 0; i < cards.length; i++){
-
-		
-
-		if(cards[i].classList.contains('active')){
-			const h1Project = document.createElement('h1');
-			const textProject = document.createElement('p');
-			const a = document.createElement('a');
-
-
-			h1Project.classList.add('title-project');
-			h1Project.innerText = project[i].name;
-
-			
-			textProject.classList.add('texto');
-			textProject.innerText = project[i].txt;
-
-			a.innerHTML = project[i].link
-			a.href =  project[i].link
-
-			txtProject.appendChild(a)
-			txtProject.appendChild(h1Project);
-			txtProject.appendChild(textProject)
-		}
-
-		
-	}
-
-
 }
 
 
