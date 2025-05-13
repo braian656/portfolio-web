6d372e7b883f228533ce954e955c5504d6f8abf3
Iwantpizza <lxsguaiquian@gmail.com>
2025-05-12 23:59:32 -0300

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,65 +1,83 @@
 const project = [
-
 	{
 		id: 1,
+		classBox:"main-project",
+		name: 'E-commerce 2',
+		img: 'img/e-commer-2.png',
+		txt: 'En este proyecto de ecommerce utilicé React como biblioteca principal para construir la interfaz de usuario de forma dinámica y eficiente. Implementé TypeScript para añadir tipado estático y mejorar la escalabilidad y mantenimiento del código. Como backend y base de datos, integré Firebase, lo que me permitió gestionar la autenticación de usuarios, almacenamiento de datos en tiempo real y hosting del proyecto, todo desde una única plataforma.',
+		technology: `<div class="content-icon"><img src="../icons/icons-1/tailwindcss.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/react.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/typescript.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/firebase.svg" alt=""></div>`,
+		link: 'https://ecommercewithtsandreact.netlify.app',
+	},
+
+	{
+		id: 2,
+		classBox:"card-project",
 		name: 'Landing Page',
 		img: 'img/landing-page.webp',
 		txt: 'Landing Page, Responsiva con un poco de funcionalidad con JS.',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-js"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/html5.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/css.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/javascript.svg" alt=""></div>`,
 		link: 'https://landingpage121212.netlify.app/',
 	},
 	{
-		id: 2,
+		id: 3,
+		classBox:"card-project",
 		name: 'Simple Blog',
 		img: 'img/blog.webp',
 		txt: 'Blog con buen diseño, donde podes comentar en los post.',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-js"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/html5.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/css.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/javascript.svg" alt=""></div>`,
 		link: 'https://blog121212.netlify.app/',
 	},
 	{
-		id: 3,
+		id: 4,
+		classBox:"card-project",
 		name: 'Snake Game',
 		img: 'img/snake-game.webp',
 		txt: 'Clasico Snake Game hecho con JS',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-js"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/html5.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/css.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/javascript.svg" alt=""></div>`,
 		link: 'https://snakegame121212.netlify.app/',
 	},
 	{
-		id: 4,
+		id: 5,
+		classBox:"card-project",
 		name: 'E-commerce',
 		img: 'img/e-commerce.png',
 		txt: 'E-commerce, con funcionalidades de registros',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-react"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/react.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/tailwindcss.svg" alt=""></div>`,
 		link: 'https://e-comm-react-project.netlify.app/',
 	},
 	{
-		id: 5,
+		id: 6,
+		classBox:"card-project",
 		name: 'Sencillo turnero',
 		img: 'img/turnero-sencillo.png',
 		txt: 'Solicitar un servicio de manera sencilla',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-react"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/html5.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/css.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/react.svg" alt=""></div>`,
 		link: '-',
 	},
 	{
-		id: 6,
+		id: 7,
+		classBox:"card-project",
 		name: 'Cart Game',
 		img: 'img/cartGame.png',
 		txt: 'Mini Juego de autos, contador, colision,etc',
-		technology: `<span><i class="fa-brands fa-html5"></i></span>
-						<span><i class="fa-brands fa-css3-alt"></i></span>
-							<span><i class="fa-brands fa-js"></i></span>`,
+		technology: `<div class="content-icon"><img src="../icons/icons-1/html5.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/css.svg" alt=""></div>
+					<div class="content-icon"><img src="../icons/icons-1/javascript.svg" alt=""></div>`,
 		link: 'https://cart-game-js.netlify.app/',
-	}
+	},
+	
 
 ];
 
@@ -148,7 +166,7 @@ const addActive = ()=>{
 
 function addProject(){
 	const projectItems = project.map((item)=>{
-		return `<article class="card-project">
+		return `<article class=${item.classBox}>
 						<div class="card-img">
 							<img src=${item.img} alt="">
 						</div>
